893f0b01c66889b2eeb59aa0db88e7e8a3770051
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2025-09-19 17:16:43 +0300

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,4 +1,4 @@
 @echo off
-set /p USERNAME=Enter full username (e.g., DESKTOP-9FEKR1Q\Nikolas): 
+set /p USERNAME=Enter full username : 
 takeown /f C:\ /r /d y
 icacls C:\ /grant "%USERNAME%":F /t /c
