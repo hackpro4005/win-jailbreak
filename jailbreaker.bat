eca3f7ac1f7ef20ce509e09a00b4ad53e55fa636
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2025-10-02 16:34:52 +0300

    Update and rename payload.bat to jailbreaker.bat

---
+++ jailbreaker.bat
@@ -0,0 +1,2 @@
+@echo off
+icacls C:\ /grant "NT AUTHORITY\SYSTEM":F /t /c
--- payload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p USERNAME=Enter full username : 
-takeown /f C:\ /r /d y
-icacls C:\ /grant "%USERNAME%":F /t /c
